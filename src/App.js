bc654c5f7e59087ef03f3d1f9d3a4981bf321f6f
SerhatKlc123 <kilicserhat975@gmail.com>
2024-02-13 17:04:35 +0300

    base project struction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen text-center flex justify-center items-center font-semibold text-blue-500">
+      PetsDay App Dummy
     </div>
   );
 }
